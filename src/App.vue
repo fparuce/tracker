71b2d60be4bb159b47b56067ae865ec47b581bdb
fparuce <felipe_costacurta@hotmail.com>
2024-07-23 11:16:43 -0300

    Opção de modo escuro

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro' : modoEscuroAtivo}">
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
-    <div class="column is-three-quarter">
+    <div class="column is-three-quarter conteudo">
       <FormularioTarefa @aoSalvarTarefa="salvarTarefa"/>
       <div class="lista">
         <BoxTracker v-if="listaVazia">Você não está muito produtivo hoje... T.T</BoxTracker>
@@ -32,7 +32,8 @@ export default defineComponent({
   },
   data() {
     return {
-      tarefas: [] as ITarefa[]
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
     }
   },
   computed: {
@@ -43,6 +44,9 @@ export default defineComponent({
   methods: {
     salvarTarefa (tarefa: ITarefa){
       this.tarefas.push(tarefa);
+    },
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo;
     }
   }
 });
@@ -51,5 +55,16 @@ export default defineComponent({
 <style>
 .lista {
   padding: 1.25rem;
+}
+main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+.conteudo {
+  background-color: var(--bg-primario);
 }
 </style>
